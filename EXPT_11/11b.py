fbdaf47ccc0c739b0d866c4227a7c0e855e739a3
sanjay-223 <sanjaysundarapandiyan@gmail.com>
2024-04-02 08:59:03 +0530

    samba

--- EXPT_11/11b.py
+++ EXPT_11/11b.py
@@ -1,13 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 x = np.linspace(0, 2*np.pi, 100)
 y = np.sin(x)
 
-
 plt.plot(x, y, marker='o', linestyle='-', color='b', label='Sine Wave')
-
 
 plt.title("Sine Wave")
 plt.xlabel("x")
